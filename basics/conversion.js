78b741820efa3ac1fc5a47ebcb3280de12e2120e
Mayuresh Pujari <159955563+marrix123@users.noreply.github.com>
2024-05-30 06:09:39 +0000

    conversionnew

--- basics/conversion.js
+++ basics/conversion.js
@@ -25,3 +25,37 @@ let someNumber = 33
 let strNumber = String(someNumber);
 console.log(strNumber);
 console.log(typeof (strNumber));
+
+
+// ********************* operations*************//
+let val = 3
+let ngvalue = -val
+console.log(ngvalue);
+
+//
+let str1 = "Hello"
+let str2 = "\t mayur"
+let str3 = str1 + str2
+console.log(str3);
+// 
+console.log("12"+3); // 123
+console.log("1"+2+2); //122
+console.log(1+2+"2"); // 32
+
+//
+let gameCounter = 100
+
+console.log((gameCounter)++);
+
+let gameCount = 100;
+
+console.log(++(gameCount));
+
+// 
+let gameCounter1 = 200
+console.log((gameCounter1)--);
+
+let gameCount1 = 200
+console.log(--(gameCount1));
+
+
