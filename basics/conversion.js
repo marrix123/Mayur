a5e4dafeadd7fb8b1e63070c4dc98166353a136b
Mayuresh Pujari <159955563+marrix123@users.noreply.github.com>
2024-05-30 05:41:20 +0000

    conversiondata

---
+++ basics/conversion.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+//"33"  => 33
+//"123abc" => NaN
+// true => 1; false=>0;
+
+let isLoggedIn = 1;
+
+let bool = Boolean(isLoggedIn);
+
+console.log(bool);
+
+// 1=> true; 0=> false
+//"" => false
+//"mayuresh" => true
+
+let someNumber = 33
+let strNumber = String(someNumber);
+console.log(strNumber);
+console.log(typeof (strNumber));
