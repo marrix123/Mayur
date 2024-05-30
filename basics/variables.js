fcdfd3e4a46a9ffc20d193e8e6aa29966ff4c255
Mayuresh Pujari <159955563+marrix123@users.noreply.github.com>
2024-05-30 05:02:21 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1233422
+let accountEmail = "mayureshpujari01@gmail.com"
+var accountPassword = "2342"
+accountCity = "jaipur"
+
+
+accountEmail = "hello@123"
+accountPassword = "4554"
+// accountId = 2 // not allowed beacuse of const
+accountCity = "mirpur"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
